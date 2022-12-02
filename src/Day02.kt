cf8c67f8f49ec3656003743f9526771bcc1c26f7
Vseslav Kasatskii <71323710+samorojy@users.noreply.github.com>
2022-12-02 18:54:15 +0200

    added solution for day 2

---
+++ src/Day02.kt
@@ -0,0 +1,32 @@
+fun main() {
+    val inputs = readInput("Day02")
+    println("inputs=${calculateScore(inputs,true)}")
+    println("topThree=${calculateScore(inputs,false)}")
+}
+
+fun calculateScore(input: List<String>, withPlay: Boolean): Int {
+    val lose = mapOf(1 to 0, 2 to 1, 0 to 2)
+    val won = mapOf(0 to 1, 1 to 2, 2 to 0)
+
+    return input.sumOf { round ->
+        val (op, result) = round.split(" ").map { it[0].code }
+        val opCode = op - 'A'.code
+        val meCode = result - 'X'.code
+
+        val (resultScore, playScore) = if (withPlay) {
+            when {
+                opCode == meCode -> 3
+                won[opCode]!! == meCode -> 6
+                else -> 0
+            } to meCode + 1
+        } else {
+            3 * meCode to when (meCode) {
+                0 -> lose[opCode]!!
+                1 -> opCode
+                else -> won[opCode]!!
+            } + 1
+        }
+
+        resultScore + playScore
+    }
+}
